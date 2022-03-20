06052d7c4e2cbe34cf977d22e541f87d7dfa520e
Nigel <nigelfernandes.contact@gmail.com>
2022-03-20 18:11:15 +0530

    set up mfa

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,19 +1,20 @@
 var express = require("express");
 var router = express.Router();
 var users = require("../public/json/users.json");
-
+const crypto = require("crypto");
+var tempUsers = users;
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.json(users);
 });
 
 router.get("/:uid", (req, res) => {
-  users = users.filter((user) => {
+  tempUsers = users.filter((user) => {
     if (user["uid"] == req.params.uid) return user;
   });
   // console.log(users.length);
-  if (users.length == 1) {
-    res.json(users);
+  if (tempUsers.length == 1) {
+    res.json(tempUsers);
   } else {
     res.status(404).json({ message: "user not found" });
   }
