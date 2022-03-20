0a64e669e81083bc7191baf3cb094b59605aaa76
Nigel <nigelfernandes.contact@gmail.com>
2022-03-20 15:51:49 +0530

    get users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var users = require("../public/json/users.json");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.json(users);
+});
+
+router.get("/:uid", (req, res) => {
+  users = users.filter((user) => {
+    if (user["uid"] == req.params.uid) return user;
+  });
+  res.json(users);
 });
 
 module.exports = router;
